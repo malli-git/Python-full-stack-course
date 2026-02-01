f1427d5dbb7a804e11c9670bcd8fcd11fef50a0e
k Mallikarjuna Reddy <mkallyam.2002@gmail.com>
2026-02-01 17:08:23 +0530

    Add files via upload

---
+++ 1.python.py
@@ -0,0 +1,4 @@
+print("Name: k.mallikarjuna reddy")
+print("course: python full stack")
+print("batch :48")
+print("Location:HYD")
---
+++ data_type.py.py
@@ -0,0 +1,13 @@
+Ticket_name=str(input("enter the Ticketname:")
+num=int(input("enter the no of tickets:")
+price=float(input("enter the total amount:")
+poojas_available=list["Abshikam","Garbalaya abshikam","Homam","KunkumaArchana"]
+Ticket_numbers=tuple("12345","234576","249454","53574")
+Documents_to_be_submitted={"Aadhar","Ticket"}
+
+print(f"Ticket_name")            
+print(f"num")
+print(f"price")
+print(f"poojas_available")
+print(f"Ticket_numbers")
+print(f"documents_to_be_submitted")  
